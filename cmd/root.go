9121ff51484aaeea43de805a6a0b3321f5c02407
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-14 15:16:35 +0200

    Validate command options

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 Obakeng Mosadi mosadiobakeng7@gmail.com
 package cmd
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -26,6 +27,7 @@ var rootCmd = &cobra.Command{
     no file is specified) to the standard output.  A line is defined as a string of characters delimited by a ⟨newline⟩
     character.  Characters beyond the final ⟨newline⟩ character will not be included in the line count.`,
 	Run: func(cmd *cobra.Command, args []string) {
+
 	},
 }
 
@@ -42,4 +44,12 @@ func init() {
 	rootCmd.Flags().StringVarP(&rootCmdOptions.numBytes, "bytes", "c", "", "The number of bytes in each input file is written to the standard output.")
 	rootCmd.Flags().StringVarP(&rootCmdOptions.numLines, "lines", "l", "", "The number of lines in each input file is written to the standard output.")
 	rootCmd.Flags().StringVarP(&rootCmdOptions.numLines, "words", "w", "", "The number of words in each input file is written to the standard output.")
+}
+
+func (r *rootOptions) validate() error {
+	if r.numBytes == "" || r.numLines == "" || r.numWords == "" {
+		slog.Error("Please provide a file path")
+	}
+
+	return nil
 }
