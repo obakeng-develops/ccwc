ab82c4633def2ef773b21b66c704db8a23f39dc8
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-14 14:56:15 +0200

    Add cmd description

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 Obakeng Mosadi mosadiobakeng7@gmail.com
-
 */
 package cmd
 
@@ -10,21 +9,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ccwc",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "wc - word, line, character, and byte count",
+	Long: `
+	The wc utility displays the number of lines, words, and bytes contained in each input file, or standard input (if
+    no file is specified) to the standard output.  A line is defined as a string of characters delimited by a ⟨newline⟩
+    character.  Characters beyond the final ⟨newline⟩ character will not be included in the line count.`,
+	Run: func(cmd *cobra.Command, args []string) {
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,15 +32,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ccwc.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
