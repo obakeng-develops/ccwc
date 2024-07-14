3c010c29ecd51dbdd591bfc9914d94fe2ffa031e
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-14 15:13:52 +0200

    Add cmd options to the root command

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type rootOptions struct {
+	numBytes string
+	numLines string
+	numWords string
+}
+
+var rootCmdOptions = &rootOptions{}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ccwc",
@@ -18,7 +26,6 @@ var rootCmd = &cobra.Command{
     no file is specified) to the standard output.  A line is defined as a string of characters delimited by a ⟨newline⟩
     character.  Characters beyond the final ⟨newline⟩ character will not be included in the line count.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 	},
 }
 
@@ -32,5 +39,7 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&rootCmdOptions.numBytes, "bytes", "c", "", "The number of bytes in each input file is written to the standard output.")
+	rootCmd.Flags().StringVarP(&rootCmdOptions.numLines, "lines", "l", "", "The number of lines in each input file is written to the standard output.")
+	rootCmd.Flags().StringVarP(&rootCmdOptions.numLines, "words", "w", "", "The number of words in each input file is written to the standard output.")
 }
